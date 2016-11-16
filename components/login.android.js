49647c319cf85b7952100442c329783cd3d600cc
Suresh.R.S <suresh.rs@experionglobal.com>
2016-11-16 18:23:39 +0530

    After designing login screen with hardcoded credentials

--- components/login.android.js
+++ components/login.android.js
@@ -3,21 +3,39 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  TextInput,
+  Button,
+  Image, 
+  ToastAndroid
 } from 'react-native';
 
 export default class Login extends Component{
+	constructor(){
+		super();
+		this.state = {
+			username : '',
+			password : ''
+		};
+	}
 
 	loginButtonPressed(){
-		this.props.navigator.push({
-			id : 'Home'
-		});
+		if(this.state.username == 'sample' && this.state.password =='123'){
+			ToastAndroid.show('Login Successful!!',ToastAndroid.SHORT);
+			this.props.navigator.push({
+				id : 'Home'
+			});
+		}
+		else{
+			ToastAndroid.show('Invalid Credentials!!',ToastAndroid.SHORT);
+		}
 	}
 
 	render(){
 		return(
 			<View style = {styles.container}>
-			<Text>Login Screen</Text>
+			<Image style = {styles.loginImage} source = {require('../images/reactNative.png')}/> 
+			<TextInput style = {styles.inputField} value = {this.state.username} onChangeText = {(username) => this.setState({username})}></TextInput>
+			<TextInput style = {styles.inputField} secureTextEntry = {true} value = {this.state.password} onChangeText = {(password) => this.setState({password})}></TextInput>
 			<Button onPress = {this.loginButtonPressed.bind(this)} title = 'Login'/>
 			</View>
 		);
@@ -28,7 +46,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems : 'center',
     backgroundColor: '#F5FCFF',
+  },
+  loginImage : {
+  	height : 100,
+  	width : 110
+  },
+  inputField : {
+  	width : 300
   }
 });
