930b4e8c9034e35cd7f8f68d39513429c88d4ecf
Suresh.R.S <suresh.rs@experionglobal.com>
2016-11-16 18:43:14 +0530

    After implementing navigation between login and home screens

--- components/login.android.js
+++ components/login.android.js
@@ -3,57 +3,32 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
-  Button,
-  Image, 
-  ToastAndroid
+  Button
 } from 'react-native';
 
 export default class Login extends Component{
-	constructor(){
-		super();
-		this.state = {
-			username : '',
-			password : ''
-		};
-	}
 
-	loginButtonPressed(){
-		if(this.state.username == 'sample' && this.state.password =='123'){
-			ToastAndroid.show('Login Successful!!',ToastAndroid.SHORT);
-			this.props.navigator.push({
-				id : 'Home'
-			});
-		}
-		else{
-			ToastAndroid.show('Invalid Credentials!!',ToastAndroid.SHORT);
-		}
-	}
+  loginButtonPressed(){
+    this.props.navigator.push({
+      id : 'Home'
+    });
+  }
 
-	render(){
-		return(
-			<View style = {styles.container}>
-			<Image style = {styles.loginImage} source = {require('../images/reactNative.png')}/> 
-			<TextInput style = {styles.inputField} value = {this.state.username} onChangeText = {(username) => this.setState({username})}></TextInput>
-			<TextInput style = {styles.inputField} secureTextEntry = {true} value = {this.state.password} onChangeText = {(password) => this.setState({password})}></TextInput>
-			<Button onPress = {this.loginButtonPressed.bind(this)} title = 'Login'/>
-			</View>
-		);
-	}
+  render(){
+    return(
+      <View style = {styles.container}>
+      <Text>Login Screen</Text>
+      <Button onPress = {this.loginButtonPressed.bind(this)} title = 'Login'/>
+      </View>
+    );
+  }
 } 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems : 'center',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  loginImage : {
-  	height : 100,
-  	width : 110
-  },
-  inputField : {
-  	width : 300
   }
 });
